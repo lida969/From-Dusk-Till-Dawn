3de4a39c957ad4fdaa2d7c28bdb3854c66b74f23
lida969 <123-4356@list.ru>
2023-05-24 23:53:34 +0300

    нихуя не работает спавнер

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
-    [SerializeField] private float _spawnInterval;
+    GameObject _position;
+    [SerializeField] private GameObject _spawnLine;
+    [SerializeField] private GameObject _spawnLine2;
 
-    [SerializeField] private int _mixX;
+    private int enemyCount = 0;
+
+
+
+    //GameObject _player;
+    //[SerializeField] private float _spawnInterval;
+
+    /*[SerializeField] private int _mixX;
     [SerializeField] private int _mixY;
     [SerializeField] private int _maxX;
     [SerializeField] private int _maxY;
@@ -13,10 +26,52 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int _height;
 
     private float _currentSpawnTimer;
+    */
 
     private void Update()
     {
-        _currentSpawnTimer += Time.deltaTime;
+        var _player = FindObjectOfType<Player>();
+        Vector3 point = _spawnLine.transform.position;
+        Vector3  point2 = _spawnLine2.transform.position;
+        
+
+        Vector3 end = new Vector3(1500f, 0, 0);
+       
+
+        //if (point.x <= end.x)
+        //{
+
+        if (_player.transform.position.x >= point.x && enemyCount != 10)
+        {
+            Spawner();
+            if (enemyCount == 10)
+            {
+                enemyCount = 0;
+            }
+
+        }
+        if (_player.transform.position.x >= point2.x && enemyCount != 10)
+        {
+
+            Spawner();
+            if (enemyCount == 10)
+            {
+                enemyCount = 0;
+            }
+
+        }
+
+        
+
+
+
+        // }
+
+
+
+
+
+        /*_currentSpawnTimer += Time.deltaTime;
 
         if (_currentSpawnTimer >= _spawnInterval)
         {
@@ -26,13 +81,32 @@ public class EnemySpawner : MonoBehaviour
             enemyInstance.transform.position = newPosition;
 
             _currentSpawnTimer = 0;
-        }
+        }*/
     }
 
-    private Vector3 GenerateStartPosition()
+    /*private Vector3 GenerateStartPosition()
     {
         var startPos = new Vector3(Random.Range(_mixX, _maxX), _height, Random.Range(_mixY, _maxY));
 
         return startPos;
+    }*/
+
+    private void Spawner()
+    {
+        
+        if (enemyCount <= 10)
+        {
+            _position = GameObject.FindWithTag("Zombie");
+            Transform _enemyPos = _position.transform;
+            Quaternion rotation = Quaternion.Euler(0, -90, 0);
+            _enemyPos.rotation = rotation;
+            //_enemyPos = new Vector3(_enemyPos.x, -90f, _enemyPos.z);
+            Destroy(_position);
+            Instantiate(_enemyPrefab, _enemyPos.position, Quaternion.identity);
+            enemyCount++;
+        }
+        
+
     }
-}
+
+} //они бегут влево от игрока. почему?
